0122ba66481640cf010609486fc6420e244f56db
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-05-05 14:01:11 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        System.out.println("Start loading ... ");
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("\n!!Thread interrupted!!");
+                return;
+            }
+            System.out.print("\r load: " + process[index++]);
+            if (index > 3) {
+                index = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
