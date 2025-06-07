09b3bd8522740e52142791839377a3bfcd7e7177
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-06-07 12:58:54 +0300

    fix issues with multithreading

--- src/main/java/ru/job4j/exam/SpamChecker.java
+++ src/main/java/ru/job4j/exam/SpamChecker.java
@@ -1,20 +1,20 @@
 package ru.job4j.exam;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class SpamChecker {
     boolean isSpam(List<Filter> filters, String text) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(6);
-        for (Filter filter : filters) {
-            if (executor.submit(() -> filter.getKeys()
-                    .stream()
-                    .map(text::contains)
-                    .reduce((aBoolean, aBoolean2) -> aBoolean || aBoolean2))
-                    .get()
-                    .get()) {
+        List<Future<Boolean>> futures = filters.stream()
+                .map(filter -> executor.submit(
+                        () -> filter.getKeys()
+                            .stream()
+                            .map(text::contains)
+                            .reduce((aBoolean, aBoolean2) -> aBoolean || aBoolean2).get()))
+                .toList();
+        for (Future<Boolean> future : futures) {
+            if (future.get()) {
                 executor.shutdownNow();
                 return true;
             }
